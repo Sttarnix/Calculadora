d6bae37cd2aa2920a00a39ad7a38aa92209952ef
Sttarnix <aiara.dnls@gmail.com>
2023-04-25 19:51:22 -0300

    Alteração na ordem das informações requisitadas e adição de Porcentagem.

--- src/Calcular/Base.java
+++ src/Calcular/Base.java
@@ -2,25 +2,31 @@ package Calcular;
 import java.util.Scanner;
 
 public class Base {
-
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		System.out.println("Iniciando Calculo");
 		
-		double valor1, valor2;
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Insira o primeiro numero");
-		valor1 = scanner.nextDouble();
-		System.out.println("Insira o segundo numero");
-		valor2 = scanner.nextDouble();
-		
-		scanner.close();
-		
 		exibirOpcoes();
+		calcular();
+	}
 	
-		int opcao = scanner.nextInt();	
+	public static void exibirOpcoes() {
+		System.out.println("Selecione a Operacao Desejada");
+		System.out.println("1 - Soma"); 
+		System.out.println("2 - Subtracao");
+		System.out.println("3 - Multiplicacao");
+		System.out.println("4 - Divisao");
+		System.out.println("5 - Porcentagem");
+	}
 	
+	public static void calcular() {
+		Scanner scanner = new Scanner(System.in);
+		int opcao = scanner.nextInt();
+		System.out.println("Insira o primeiro numero");
+		double valor1 = scanner.nextDouble();
+		System.out.println("Insira o segundo numero");
+		double valor2 = scanner.nextDouble();
+		
 		switch(opcao) {
 	
 		case 1:
@@ -39,19 +45,15 @@ public class Base {
 			double resultadoDivisao = divisao(valor1, valor2);
 			System.out.println("Resultado da divisao " + resultadoDivisao);
 			break;
+		case 5:
+			double resultadoPorcentagem = porcentagem (valor1, valor2);
+			System.out.println("O resultado da porcentagem e: " + resultadoPorcentagem);
+			break;
 		default:
 			System.out.println("Operacao Invalida");
 			break;
 		}
-	}
-	
-	public static void exibirOpcoes() {
-		System.out.println("Selecione a Operacao Desejada");
-		System.out.println("1 - Soma"); 
-		System.out.println("2 - Subtracao");
-		System.out.println("3 - Multiplicacao");
-		System.out.println("5 - Divisao");
-		System.out.println("Opcao: ");
+		scanner.close();
 	}
 	
 	public static void obterEntradas () {
@@ -76,4 +78,8 @@ public class Base {
 		double resultado = valor1 / valor2;
 		return resultado;
 	}	
+	public static double porcentagem(double valorOriginal, double porcentagem) {
+		double resultado = (valorOriginal * porcentagem) / 100;
+		return resultado;
+	}
 }
